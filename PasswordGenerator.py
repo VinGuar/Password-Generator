4e4b08cddde66a6a7b21cac0909940e6989384fe
VinGuar <126090788+VinGuar@users.noreply.github.com>
2023-03-01 04:17:15 +0000

    big save

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,9 @@
+from random import randint
+import random
+
 amount = []
 checker = 0
+password = ""
 while checker <= 127:
     amount.append(checker)
     checker += 1
@@ -7,10 +11,18 @@ while checker <= 127:
 
 while True:
     numberOfChar = input("What length do you want the password (up to 127)? ")
-    if int(numberOfChar) in amount:
+    try:
+        int(numberOfChar)
+    except:
+        print("Please type an integer from 0-127 in number form. ", end="")
+        continue
+
+    if (int(numberOfChar)>=0 and int(numberOfChar)<=127):
         break
     else:
         print("Please type a number from 0-127 in number form. ", end="")
+        
+            
 
 def check(data):
     if data in ("Y", "y"):
@@ -34,7 +46,47 @@ numbers = input("Do you want numbers? Y/N: ")
 numbers = check(numbers)
 
 
-print(upperCase)
-print(lowerCase)
-print(symbols)
-print(numbers)
+def passMaker(up, low, sym, num, numChar):
+
+    randNeeded = 0
+    inputter = [up, low, sym, num]
+
+    for i in range(4):
+        if inputter[i] == True:
+            randNeeded += 1
+
+    randNumbers = [0]*randNeeded
+
+
+    for i in range(numChar):
+        randNumbers[randint(0,randNeeded-1)] +=1
+
+    return randNumbers
+
+randomNumbers = passMaker(upperCase, lowerCase, symbols, numbers, int(numberOfChar))
+print(randomNumbers)
+if upperCase == True:
+    for x in range(randomNumbers[0]):
+        password += chr(random.randint(65,90))
+
+if lowerCase == True:
+    for x in range(randomNumbers[1]):
+        password += chr(random.randint(97,122))
+
+if symbols == True:
+    for x in range(randomNumbers[2]):
+        password += chr(random.randint(33,152))
+
+if numbers == True:
+    for x in range(randomNumbers[3]):
+        password += chr(random.randint(48,57))
+
+print(password)
+passList = list(password)
+random.shuffle(passList)
+password = "".join(passList)
+print(password)
+
+
+
+
